44f1f661a7fe3bad26667d29996c027ebed5d59f
Alex <alex.gyumushyan@gmail.com>
2021-11-26 23:24:47 -0500

    grid class basics

--- Pathfinder/Form1.cs
+++ Pathfinder/Form1.cs
@@ -12,9 +12,40 @@ namespace Pathfinder
 {
     public partial class Form1 : Form
     {
+        
+        Grid newGrid = new Grid(50, 50, 20);
+        
         public Form1()
         {
             InitializeComponent();
+            this.Width = 500;
+            this.Height = 500;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            newGrid.Draw(e);
+            
+        }
+
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            // replace 50 with an actual variable 
+            if (e.X >= 50 && 
+                e.X <= 50 + 200 &&
+                e.Y >= 50 &&
+                e.Y <= 50 + 200)
+            {
+                newGrid.click(e.X, e.Y);
+            } else
+            {
+                newGrid.click(-1, -1);
+            }
+
+            Refresh();
         }
     }
 }
---
+++ Pathfinder/Grid.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Pathfinder
+{
+    internal class Grid
+    {
+        PointF gridStart;
+        int cellWidth;
+
+        Pen myPen = new Pen(Color.Black);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+
+       
+        bool drawCurrent = true;
+        int verticalCell;
+        int horizontalCell;
+        
+        int startLength = 10;
+        public Grid(int x, int y, int width)
+        {
+            this.gridStart.X = x;
+            this.gridStart.Y = y;
+            this.cellWidth = width;
+        }
+
+        public void click(int x, int y)
+        {
+            if (x == -1 && y == -1)
+            {
+                drawCurrent = false;
+            } else
+            {
+                drawCurrent = true; 
+
+
+                this.horizontalCell = (int)Math.Floor((x - gridStart.X) / 20);
+                this.verticalCell = (int)Math.Floor((y - gridStart.Y) / 20);
+            }
+        }
+
+        public void Draw(PaintEventArgs e)
+        {
+            
+            Graphics g = e.Graphics;
+
+            for (int i = 0; i <= startLength; i++)
+            {
+                //vertical    
+                g.DrawLine(myPen, gridStart.X + (i * cellWidth), gridStart.Y, gridStart.X + (i * cellWidth), gridStart.Y + 200);
+
+                //horizontal                               
+                g.DrawLine(myPen, gridStart.X, gridStart.Y + (i * cellWidth), gridStart.X + 200, gridStart.Y + (i * cellWidth));
+            }
+
+            if (drawCurrent)
+            {
+
+                g.FillRectangle(redBrush, gridStart.X + (horizontalCell * 20) + 1, gridStart.Y + (verticalCell * 20) + 1, 19, 19);
+            }
+        }
+
+    }
+}
